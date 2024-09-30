859c0aed060ddece0d13752f5ded82fcb5ee1ea4
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-01 01:56:50 +0300

    Update ResultModal.jsx

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const ResultModal = () => {
-  return <div>ResultModal</div>;
+const ResultModal = ({ result, targetTime }) => {
+  return (
+    <dialog>
+      <h2>You {result}</h2>
+      <p>
+        the target time was <strong>{targetTime}</strong>
+      </p>
+    </dialog>
+  );
 };
 
 export default ResultModal;
