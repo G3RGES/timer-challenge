04a748c91642d8edcd7a80211ad6ba3065a01bb9
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-01 02:14:12 +0300

    Update ResultModal.jsx

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -2,11 +2,17 @@ import React from "react";
 
 const ResultModal = ({ result, targetTime }) => {
   return (
-    <dialog>
+    <dialog className="result-modal" open>
       <h2>You {result}</h2>
       <p>
-        the target time was <strong>{targetTime}</strong>
+        the target time was <strong>{targetTime} seconds</strong>
+        <p>
+          You stopped the timer with <strong>X seconds left</strong>
+        </p>
       </p>
+      <form method="dialog">
+        <button type="submit">Close</button>
+      </form>
     </dialog>
   );
 };
