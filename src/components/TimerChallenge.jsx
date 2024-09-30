b72d60e01ab7c02dee4ab96078d6e2b12a055904
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-09-30 03:01:24 +0300

    Create TimerChallenge.jsx

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TimerChallenge = ({ title, targetTime }) => {
+  return (
+    <section className="challenge">
+      <h2>{title} </h2>
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ?? "s"}
+      </p>
+      <p>
+        <button>Start/Stop Challenge</button>
+      </p>
+    </section>
+  );
+};
+
+export default TimerChallenge;
