a2ec71c2d1a01eed8589f6529cb2947107052d32
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-09-30 03:27:25 +0300

    Update TimerChallenge.jsx

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TimerChallenge = ({ title, targetTime }) => {
+  const [timerExpire, setTimerExpire] = useState(false);
+  const [timerStart, setTimerStart] = useState(false);
+
+  const handleStart = () => {
+    setTimerStart(true);
+
+    setTimeout(() => {
+      setTimerExpire(true);
+    }, targetTime * 1000);
+  };
+
   return (
     <section className="challenge">
       <h2>{title} </h2>
+      {timerExpire && <p>Timer Expired, You lost</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start/Stop Challenge</button>
+        <button onClick={handleStart}>
+          {timerStart ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p className="">Time is running... / Timer inactive</p>
+      <p className={timerStart ? "active" : ""}>
+        {timerStart ? "Time is running..." : "Timer inactive"}
+      </p>
     </section>
   );
 };
